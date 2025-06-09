626bc80e03e4215d3c20867f18dfce42dccbbc9f
joaovsantoscarvalho <carvalho.joao08@escola.pr.gov.br>
2025-06-09 07:25:20 -0300

    .

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 const mario = document.querySelector(".mario")
 const pipe = document.querySelector(".pipe")
+
+const jump = () => {
+    mario.classList.add("jump");
+
+    setTimeout(() => {
+        mario.classList.remover("jump");
+    }
+    ,500);
+}
+
+document.addEventListener("keydown", jump);
